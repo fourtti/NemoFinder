4482968cf453bc05c31620815735417beb159998
Markus Keinänen <markusvk@metropolia.fi>
2017-09-13 11:13:51 +0300

    Lisättiin menu.html ja siihen napit, joihin voi alkaa lisäillä redirectejä. Poistettiin pari turhaa filea, lisättiin images-kansio

--- public/javascripts/nemofinder.js
+++ public/javascripts/nemofinder.js
@@ -1,11 +1,25 @@
-var app = angular.module('NemoFinder', ['ngRoute']);
+var app = angular.module('NemoFinder', ['ngResource','ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/', {
-            templateUrl: 'partials/home.html'
-        })
-        .otherwise({
+            templateUrl: 'partials/home.html',
+            controller: 'HomeCtrl'
+        }).when('/login', {
+            templateUrl: 'partials/login.html',
+            controller: 'LoginCtrl'
+
+        }).otherwise({
             redirectTo: '/'
         });
 }]);
+
+app.controller('HomeCtrl', ['$scope', '$resource', 
+	function($scope, $resource){
+
+	}]);
+
+app.controller('LoginCtrl', ['$scope', '$resource', 
+	function($scope, $resource){
+
+	}]);
