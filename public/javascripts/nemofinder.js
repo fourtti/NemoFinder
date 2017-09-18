16a89620e9325a77ddbece991d10313297367554
fourtti <fourtti@gmail.com>
2017-09-18 13:21:06 +0300

    basic login

--- public/javascripts/nemofinder.js
+++ public/javascripts/nemofinder.js
@@ -3,7 +3,10 @@ var app = angular.module('NemoFinder', ['ngRoute']);
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
         .when('/', {
-            templateUrl: 'partials/home.html'
+            templateUrl: 'partials/login.html'
+        })
+        .when('login', {
+        	templateUrl: 'partials/login.html'
         })
         .otherwise({
             redirectTo: '/'
