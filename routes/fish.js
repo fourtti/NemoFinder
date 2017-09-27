f569276cf91cb6b6464b80979be9b900273df920
theonelegion <niklas.kuusisto95@gmail.com>
2017-09-27 16:35:12 +0300

    added temporary test recource for adding of Fishdata to the database

--- routes/fish.js
+++ routes/fish.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+let fishDataCtrl = require('../models/Db_controllers/fishdataCtrl');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -13,6 +14,20 @@ router.put('/local/X/:xParam/Y/:yParam/weight/:weightParam', function(req, res) 
 		weight : weightParam
 	};
 	res.json(newLocalFish);
+});
+
+//Test route for adding a single fishdata to the database without owner
+// REMEMBER TO DELETE BEFORE RETURN OF PROJECT AND REPLACE WITH CORRECT IMPLEMENTATION AFTER ATHENTICATION IS ADDED
+router.post('/add/:long/:lat/:depth',function(req,res){
+	
+	let long = req.params.long;
+	let lat = req.params.lat;
+	let depth = req.params.depth;
+
+	fishDataCtrl.createFishdata([long,lat],depth,"Unknown",true);
+	res.status(200);
+	res.json({message: 'FishData saved!'});
+
 });
 
 module.exports = router;
