ab87e8ea904f92fbf30c0d3928e19df33e95fda2
theonelegion <niklas.kuusisto95@gmail.com>
2017-10-02 21:00:04 +0300

    added all API routes and Map Model controller functions

--- models/db.js
+++ models/db.js
@@ -1,8 +1,8 @@
 const mongoose = require( 'mongoose' );
 
 
-const dbUrl = "mongodb://localhost/NemoFinder"; // link to local server
-//const dbUrl = "mongodb://nemo:soundsfishy@ds151024.mlab.com:51024/nemofinder"
+//const dbUrl = "mongodb://localhost/NemoFinder"; // link to local server
+const dbUrl = "mongodb://nemo:soundsfishy@ds151024.mlab.com:51024/nemofinder"
 
 
 let gracefulShutdown;
--- models/schemas/map.js
+++ models/schemas/map.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const map = new Schema({
+	name: {type:String, required: true},
 	fishdata: [{ type: Schema.Types.ObjectId }],
-	name: {type:String, unique: true, required: true},
 	timeStamp: {type: Date, "default": Date.now },
 	owner:{ type: Schema.Types.ObjectId },
 	private: { type: Boolean }
